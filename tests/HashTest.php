482c79411be622de584f4db29c61c532335dcd56
MisterioRee <Rehan@awj.ae>
2019-07-18 11:58:41 +0400

    Added Discription to test cases

--- tests/HashTest.php
+++ tests/HashTest.php
@@ -13,8 +13,11 @@ class HashTest extends TestCase
      */
     public function testExample()
     {
+        //$response = $this->call('GET', '/hash', ['Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvbG9naW4iLCJpYXQiOjE1NjM0MzYzODUsImV4cCI6MTU2MzQzOTk4NSwibmJmIjoxNTYzNDM2Mzg1LCJqdGkiOiIySWF6MFpSblI5Y25vQllnIiwic3ViIjoyMCwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSJ9.I-tWzjh85TKuaWQkhG2h4XTLsb0WqCwjdTUciGHhMEU']);
+
         $response = $this->call('GET', '/hash');
 
+        //$this->assertEquals(200, $response->status());
         $this->assertEquals(401, $response->status());
     }
 }
